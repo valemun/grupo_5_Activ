44ffed480ffc56e6d39939f2823d7d0219ce93a8
valemun <valeria.munoz@hotmail.com>
2021-10-13 01:20:07 -0700

    product create y cart

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,13 @@
 const path = require( "path" );
+let productos = require( "../data/product.json" );
+
 
 const controller = {
     profile: (req, res) => {
         res.render( "users/register" );
     },
     cart: (req, res) => {
-        res.render( "users/cart" );
+        res.render( "users/cart", {"productos":productos} );
     },
     login: (req, res) => {
         res.render( "users/login" );
